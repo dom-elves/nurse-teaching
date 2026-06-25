40c01dad8ecd57090a059283a0bbed649e614064
dom-elves <79201054+dom-elves@users.noreply.github.com>
2026-06-25 14:05:28 +0100

    seed in mig as advised per nativephp

---
+++ database/migrations/2026_06_25_125710_seed_app_settings.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Dom',
+                'email' => 'dom@example.com',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'Ellie',
+                'email' => 'ellie@example.com',
+                'password' => bcrypt('password')
+            ],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
