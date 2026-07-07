d007c83e47688adff586cdb71f0bae9b7b0aa9d8
dom-elves <79201054+dom-elves@users.noreply.github.com>
2026-07-07 15:55:32 +0100

    basic image picker, no free flux comopnent exists

--- resources/views/pages/⚡create-slide.blade.php
+++ resources/views/pages/⚡create-slide.blade.php
@@ -50,28 +50,58 @@ new class extends Component
 --}}
 <div>
     <h1 class="font-medium">Create a slide</h1>
-    <form method="POST" wire:submit="create" class="flex flex-col space-y-6 mb-12">
+    <form method="POST" wire:submit="create" class="flex flex-col space-y-6 my-12">
         {{-- question selection --}}
-        <flux:select wire:model="slide.question_id">
-            <flux:select.option
-                value=""
-            >
-                {{ __('Select a question') }}
-            </flux:select.option>
-                @foreach ($this->questions as $question)
-                    <flux:select.option
-                        value="{{ $question->id }}"
-                    >
-                        {{ $question->text }}
-                    </flux:select.option>
+        <div>
+            <h2>Question</h2>
+            <flux:select wire:model="slide.question_id">
+                <flux:select.option
+                    value=""
+                >
+                    {{ __('Select a question') }}
+                </flux:select.option>
+                    @foreach ($this->questions as $question)
+                        <flux:select.option
+                            value="{{ $question->id }}"
+                        >
+                            {{ $question->text }}
+                        </flux:select.option>
+                    @endforeach
+            </flux:select>
+            @error('slide.question_id')
+                <p class="text-sm text-red-500">{{ $message }}</p>
+            @enderror
+        </div>
+        {{-- image selection, no free flux equivalent so had to make a basic one --}}
+        <div>
+            <h2>Image</h2>
+            <div class="flex flex-wrap gap-4" x-data="{ selected: null }">
+                @foreach ($this->images as $image)
+                    <div class="relative flex flex-col group cursor-pointer" style="max-height:200px">
+                        <img
+                            id="{{ $image->id }}"
+                            src="{{ route('media', $image->path) }}" 
+                            alt="{{ $image->title }}"
+                            style="max-height:200px;border:1px solid black"
+                            :class="{ 'opacity-25': selected === {{ $image->id }} }"
+                            x-on:click="$wire.set('slide.image_id', {{ $image->id }}); selected = {{ $image->id }}"
+                            >
+                        <p
+                            :class="{ 'visible': selected === {{ $image->id }} }" 
+                            class="absolute text-center bottom-0 right-0 left-0 invisible 
+                                  group-hover:visible opacity-80 bg-black text-white text-sm"
+                        >
+                            {{ $image->title }}
+                        </p>
+                    </div>
                 @endforeach
-        </flux:select>
-        @error('slide.question_id')
-            <p class="text-sm text-red-500">{{ $message }}</p>
-        @enderror
-
-        {{-- image selection --}}
-
+                {{ $this->slide['question_id']}}
+                {{ $this->slide['image_id'] }}
+            </div>
+            @error('slide.image_id')
+                <p class="text-sm text-red-500">{{ $message }}</p>
+            @enderror
+        </div>
 
         {{-- option selection --}}
 
